15736f482eb0c17e516a711fa1e17fc09ff982ee
Peter Browse <peter.browse@bmbagency.com>
2013-04-05 20:24:18 +0100

    Initial Working Commit

---
+++ public/js/client.js
@@ -0,0 +1,55 @@
+var socket = io.connect('http://localhost:8080');
+
+var totalNumber = 0,
+	totalMUFC 	= 0,
+	totalCFC 	= 0,
+	$percentageOfCFC,
+	$percentageOfMUFC;
+
+$(document).ready(function() {
+	$right = $('.right');
+	$left = $('.left');
+
+	socket.on('tweet', function(tweet){
+		//console.log(tweet.entities.hashtags[0].text);
+		for (var i=0;i<tweet.entities.hashtags.length;i++) {
+			if(tweet.entities.hashtags[i].text.toLowerCase() == 'cfc') {
+				totalNumber ++;
+				totalCFC ++;
+				//console.log('cfc');
+				CFpercentage = ((totalCFC/totalNumber)*100);
+				MUpercentage = ((totalMUFC/totalNumber)*100);
+				
+				console.log('CFC Tweet: MU ' + MUpercentage + '% CF ' + CFpercentage + '%');
+				$('.left').css('width',CFpercentage + '%');
+				$('.right').css('width',MUpercentage + '%');
+			}
+	
+			if(tweet.entities.hashtags[i].text.toLowerCase() == 'mufc') {
+				totalNumber ++;
+				totalMUFC ++;
+				//console.log('mufc');
+				
+				CFpercentage = ((totalCFC/totalNumber)*100);
+				MUpercentage = ((totalMUFC/totalNumber)*100);
+				
+				console.log('MUFC Tweet: MU ' + MUpercentage + '% CF ' + CFpercentage + '%');
+				$('.left').css('width',CFpercentage + '%');
+				$('.right').css('width',MUpercentage + '%');
+			}
+		}
+	});
+});
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i].toLowerCase() === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var red = '#c20000'; //white text
+var yellow = '#ffcc00'; //green text #006e00;
---
+++ server.js
@@ -0,0 +1,57 @@
+//console colours
+var	blue  	= '\033[34m',
+	green 	= '\033[32m',
+	red   	= '\033[31m',
+	yellow 	= '\033[33m',
+	reset 	= '\033[0m';
+
+console.log(blue+'Twitter Scrum App Starting');
+
+var express = require('express')
+,	http 	= require('http')
+,	app 	= express()
+,	Twit	= require('twit')
+,	server 	= http.createServer(app)
+,	io		= require('socket.io').listen(server);
+
+app.configure('development', function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.logger());
+});
+
+app.configure('production', function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler());
+});
+
+server.listen(process.env.PORT || 8080, function (err) {
+  if (err) {
+    throw err;
+  }
+
+  console.log(green+'info: '+reset+'Express server started on '+yellow+'%s:'+yellow+'%s'+reset+'.', server.address().address, server.address().port);
+  console.log(green+'info: '+reset+'App running in '+yellow+process.env.NODE_ENV+reset+' mode.');
+});
+
+var T = new Twit({
+    consumer_key:         'cNHlEqJVNtR77McnTRK1w'
+  , consumer_secret:      'bDSkcTrSkpox27J3rOhNhvz8NH4cgbI4qFkWUTpwplU'
+  , access_token:         '311722405-2jPxV0CqF5DeD2tdS6bt3G0nyc1WQtvCcR3APM9R'
+  , access_token_secret:  'bI58z5h3Kaf4P4DqbWqxpZo38zgcsRkchjr5pHlw10'
+});
+
+var stream = T.stream('statuses/filter', {track: ['#cfc','#mufc']});
+
+io.configure(function(){
+        io.enable('browser client minification');  // send minified client
+        io.enable('browser client etag');          // apply etag caching logic based on version number
+        io.enable('browser client gzip');          // gzip the file
+        io.set('log level', 1);                    // reduce logging
+});
+
+io.on('connection', function(socket){
+	stream.on('tweet', function (tweet) {
+		socket.emit('tweet', tweet);
+	});
+});
